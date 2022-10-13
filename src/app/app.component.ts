7818cb96b7b5b6ae3b0e1a5caabccc5126c858d3
OlehUrleeh2105 <gachcock@gmail.com>
2022-10-13 17:26:10 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-basics';
+  search = '';
+  searchField = 'title';
+
+  posts: Post[] = [
+    { title: 'Beer', text: 'The best' },
+    { title: 'Bread', text: 'The best 2' },
+    { title: 'JS', text: 'The best 3' },
+  ];
 }
